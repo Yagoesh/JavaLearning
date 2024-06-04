b58b655224c8895f0adc050d0eae42559dbdfb51
Yogesh Samtani <yogeshsamtani@gmail.com>
2024-06-04 18:40:29 +0200

    Start basic

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+  public void main(String[] args) {
+    final String Name = "Yogesh";
+
+    float age = 30.45f;
+    float refined = (float) age;
+    System.out.println(refined);
+
+    if (Name == "Yogesh" && age > 20) {
+      System.out.println("My name is " + Name + " and my age is " + age);
+    
+      
+    } else {
+      System.out.println("no hay nombre");
+      
+    }
+
+  }
+}
+
+
